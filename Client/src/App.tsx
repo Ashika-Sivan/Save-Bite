669f83118410de2db523a4519497feb16ddfc457
Ashika-Sivan <ashikasivan07@gmail.com>
2026-09-11 16:33:26 +0530

    fix: remove rogue useSocket import to fix CI build without pulling unrelated code

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,9 @@
 
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
-import { useSocket } from "./hooks/useSocket";
 import InstallPWAPrompt from "./components/common/InstallPWAPrompt";
 
 function App() {
-  useSocket();
 
   return (
     <>
